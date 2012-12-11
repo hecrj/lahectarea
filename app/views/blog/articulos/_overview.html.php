e20109538d096128c4967338c2cb78e77236b39b
Héctor <hector0193@gmail.com>
2012-12-11 18:14:24 +0100

    Removed three dots of #disqus_thread link.

--- app/views/blog/articulos/_overview.html.php
+++ app/views/blog/articulos/_overview.html.php
@@ -3,7 +3,7 @@
 		<a href="/articulo/<?= $post->permalink ?>"><?= $post->title ?></a>
 		<small>
 			<?= $this->helper('time')->getFormatted($post->published_at) ?>
-			<a href="/articulo/<?= $post->permalink ?>#disqus_thread">...</a>
+			<a href="/articulo/<?= $post->permalink ?>#disqus_thread"></a>
 		</small>
 	</h4>
 	<?= $this->helper('markdown')->translateSimple($post->description) ?>
