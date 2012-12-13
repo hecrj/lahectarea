7ca98e04c5fbb3ee166f2d456e17085bb269aee0
Héctor <hector0193@gmail.com>
2012-12-13 15:26:22 +0100

    Some fixes on production.

--- app/views/blog/articulos/_overview.html.php
+++ app/views/blog/articulos/_overview.html.php
@@ -3,6 +3,7 @@
 		<a href="/articulo/<?= $post->permalink ?>"><?= $post->title ?></a>
 		<small>
 			<?= $this->helper('time')->getFormatted($post->published_at) ?>
+			&nbsp;
 			<a href="/articulo/<?= $post->permalink ?>#disqus_thread"></a>
 		</small>
 	</h4>
