4d66c88e1b9a4b8151214ef900b763046b93e3a0
Héctor <hector0193@gmail.com>
2013-01-17 20:29:55 +0100

    Posts share bubble not appearing normally fixed.

--- app/views/blog/articulos/_share.html.php
+++ app/views/blog/articulos/_share.html.php
@@ -1,11 +1,11 @@
 <!-- AddThis Button BEGIN -->
 <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-    <a class="addthis_button_preferred_1"></a>
-    <a class="addthis_button_preferred_2"></a>
-    <a class="addthis_button_preferred_3"></a>
-    <a class="addthis_button_preferred_4"></a>
-    <a class="addthis_button_compact"></a>
-    <a class="addthis_counter addthis_bubble_style"></a>
+	<a class="addthis_button_preferred_1"></a>
+	<a class="addthis_button_preferred_2"></a>
+	<a class="addthis_button_preferred_3"></a>
+	<a class="addthis_button_preferred_4"></a>
+	<a class="addthis_button_compact"></a>
+	<a class="addthis_counter addthis_bubble_style"></a>
 </div>
 <!-- AddThis Button END -->
-<?php $this->helper('includes')->external('js', 'http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-5043f32c1fb62ad0'); ?>
+<?php $this->helper('includes')->external('js', 'http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5043f32c1fb62ad0'); ?>
